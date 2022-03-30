2617fffe94e0a53caf287c526c0c3b9ba08da1f4
Rsmile310 <rsmile310@gmail.com>
2022-03-30 10:00:54 +0900

    responsive is added

--- src/components/alltrades.js
+++ src/components/alltrades.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {crypto, addr, type} from "../assets/real-time.json";
 
 function AllTrades() {
+    const [sampleData, setSampleData] = useState([]);
+    useEffect(() => {
+        const sampleArr = [];
+        const cLength = crypto.length-1;
+        const aLength = addr.length-1;
+        const tLength = type.length-1;
+        // eslint-disable-next-line
+        [...Array(50)].map((a,i) => {
+            const row = {
+                time: `${((Math.random()*23)+1).toFixed(0)}:${((Math.random()*59)+1).toFixed(0)}`,
+                pair: crypto[(Math.random()*cLength).toFixed(0)],
+                addr: addr[(Math.random()*aLength).toFixed(0)],
+                type: type[(Math.random()*tLength).toFixed(0)],
+                price: (Math.random()*220.00).toFixed(3),
+                lev: `${(Math.random()*50 + 120).toFixed(0)}x`,
+                coll: (Math.random()*40.00).toFixed(2),
+                pnl: Math.random()*100%2===0 ? '' : Math.random()*100 > 10 ? ('+'+(Math.random()*100.0).toFixed(2)) : ('-'+(Math.random()*100.0).toFixed(2)),
+                percent: Math.random()*100%2===0 ? '' : Math.random()*100 > 10 ? ('+'+(Math.random()*100.0).toFixed(2)+'%') : ('-'+(Math.random()*100.0).toFixed(2)+'%'),
+            }
+            sampleArr.push(row);
+        })
+
+        setSampleData(sampleArr);
+    }, [])
     return (
         <div className="card m-3">
            <div className="card-header">
@@ -21,61 +46,19 @@ function AllTrades() {
                         <div>%</div>
                     </div>
                     <div className="table-body">
-                        <div className="trade-row">
-                            <div>17:39</div>
-                            <div>ETH/USD</div>
-                            <div>D0AE</div>
-                            <div>SL</div>
-                            <div>3410.00</div>
-                            <div>5x</div>
-                            <div>498.0</div>
-                            <div>+0.1</div>
-                            <div>+0.0%</div>
-                        </div>
-                        <div className="trade-row">
-                            <div>17:39</div>
-                            <div>ETH/USD</div>
-                            <div>D0AE</div>
-                            <div>SL</div>
-                            <div>3410.00</div>
-                            <div>5x</div>
-                            <div>498.0</div>
-                            <div>+0.1</div>
-                            <div>+0.0%</div>
-                        </div>
-                        <div className="trade-row">
-                            <div>17:39</div>
-                            <div>ETH/USD</div>
-                            <div>D0AE</div>
-                            <div>SL</div>
-                            <div>3410.00</div>
-                            <div>5x</div>
-                            <div>498.0</div>
-                            <div>+0.1</div>
-                            <div>+0.0%</div>
-                        </div>
-                        <div className="trade-row">
-                            <div>17:39</div>
-                            <div>ETH/USD</div>
-                            <div>D0AE</div>
-                            <div>SL</div>
-                            <div>3410.00</div>
-                            <div>5x</div>
-                            <div>498.0</div>
-                            <div>+0.1</div>
-                            <div>+0.0%</div>
-                        </div>
-                        <div className="trade-row">
-                            <div>17:39</div>
-                            <div>ETH/USD</div>
-                            <div>D0AE</div>
-                            <div>SL</div>
-                            <div>3410.00</div>
-                            <div>5x</div>
-                            <div>498.0</div>
-                            <div>+0.1</div>
-                            <div>+0.0%</div>
-                        </div>
+                        {sampleData.map((item, index) => 
+                            <div className="trade-row" key={index}>
+                                <div>{item.time}</div>
+                                <div>{item.pair}</div>
+                                <div>{item.addr}</div>
+                                <div className='rising-color'>{item.type}</div>
+                                <div>{item.price}</div>
+                                <div>{item.lev}</div>
+                                <div>{item.coll}</div>
+                                <div className={(Math.random()*100)<40? 'fall-color': 'rising-color'}>{item.pnl}</div>
+                                <div className={(Math.random()*100)<40? 'fall-color': 'rising-color'}>{item.percent}</div>
+                            </div>
+                        )}
                     </div>
                 </div>
            </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from '../assets/logo.png';
-import {FaExchangeAlt, FaPowerOff} from "react-icons/fa"
+import {FaExchangeAlt, FaPowerOff, FaBars, FaTimes} from "react-icons/fa"
 
 function Navbar() {
+
+    const [isShow, setIsShow] = useState(false)
+
     return (
         <div className="navbar">
             <div className="navbar-left">
@@ -25,6 +28,21 @@ function Navbar() {
                 </li>
                 <li>
                     <a className="button btn-pink" href="/decentralized-trading/">Connect Wallet <FaPowerOff className="reg-circle-icon" /></a>
+                </li>
+            </ul>
+            {isShow ? <FaTimes className="hamburger" onClick={() => setIsShow(!isShow)} /> : <FaBars className="hamburger" onClick={() => setIsShow(!isShow)} />}
+
+            <ul className={`mobile-navbar ${isShow?'show':'hide'}`}>
+                <li><a className="menu-link" href="/decentralized-trading/">Platform</a></li>
+                <li><a className="menu-link" href="/decentralized-trading/">Practice</a></li>
+                <li><a className="menu-link" href="/decentralized-trading/">Staking</a></li>
+                <li><a className="menu-link" href="/decentralized-trading/">Statistics</a></li>
+                <li><a className="menu-link" href="/decentralized-trading/">Documentation</a></li>
+                <li>
+                    <a className="button btn-pink menu-link" href="/decentralized-trading/">Buy $LVRJ" <FaExchangeAlt className="reg-circle-icon" /></a>
+                </li>
+                <li>
+                    <a className="button btn-pink menu-link" href="/decentralized-trading/">Connect Wallet <FaPowerOff className="reg-circle-icon" /></a>
                 </li>
             </ul>
         </div>
