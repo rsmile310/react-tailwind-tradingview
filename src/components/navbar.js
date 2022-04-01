3d30f3a7f50880e43b0d6b42b7382abd02a8823d
Rsmile310 <rsmile310@gmail.com>
2022-04-01 23:26:54 +0900

    light/night toggle button on mobile view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Switch from "react-switch";
-import {FaExchangeAlt, FaPowerOff} from "react-icons/fa"
+import {FaExchangeAlt, FaPowerOff, FaAngleLeft, FaAngleRight} from "react-icons/fa"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar({ mode, setMode, onShowSearch }) {
 
-    // const [isShow, setIsShow] = useState(false);
+    const [isShow, setIsShow] = useState(false);
     useEffect(() => {
         const r = document.querySelector(':root');
         if (!mode) {
@@ -29,59 +29,68 @@ function Navbar({ mode, setMode, onShowSearch }) {
     }
 
     return (
-        <div className={`navbar ${mode? 'dark': 'light'}`}>
-            {/* <FaUserCircle className="mobile-view-icons" /> */}
-            <img src="/icons/profile.svg" alt="profile" className="mobile-view-icons" />
-            <div className="navbar-left">
-                <div className="navbar-logo" onClick={() => onShowSearch(true)}>
-                     <FontAwesomeIcon icon={faSearch} className="navbar-search-icon" />
-                     Search markets here
-                </div>
-                <div className="navbar-menu">
-                    <a className="menu-link" href="/decentralized-trading/">Platform</a>
-                    <a className="menu-link" href="/decentralized-trading/">Practice</a>
-                    <a className="menu-link" href="/decentralized-trading/">Staking</a>
-                    <a className="menu-link" href="/decentralized-trading/">Statistics</a>
-                    <a className="menu-link" href="/decentralized-trading/">Documentation</a>
+        <div>
+            <div className={`mobile-toggle-panel ${isShow?'show':'hide'}`}>
+                <div className="toggle-button-group">
+                    {isShow ? <FaAngleRight className="toggle-arrow" onClick={() => setIsShow(!isShow)} /> : <FaAngleLeft className="toggle-arrow" onClick={() => setIsShow(!isShow)} />}
+                    <span>Dark Mode</span> <Switch onChange={() => handleChangeMode(!mode)} checked={mode} />
                 </div>
             </div>
-
-            <ul className="menu-btns">
-                <li>
-                    <div className="toggle-button-group">
-                        <span>Dark Mode</span> <Switch onChange={() => handleChangeMode(!mode)} checked={mode} />
+            <div className={`navbar ${mode? 'dark': 'light'}`}>
+                {/* <FaUserCircle className="mobile-view-icons" /> */}
+                <img src="/icons/profile.svg" alt="profile" className="mobile-view-icons" />
+                <div className="navbar-left">
+                    <div className="navbar-logo" onClick={() => onShowSearch(true)}>
+                        <FontAwesomeIcon icon={faSearch} className="navbar-search-icon" />
+                        Search markets here
                     </div>
-                </li>
-                <li>
-                    <a className="button btn-pink" href="/decentralized-trading/">Buy $LVRJ" <FaExchangeAlt className="reg-circle-icon" /></a>
-                </li>
-                <li>
-                    <a className="button btn-pink" href="/decentralized-trading/">Connect Wallet <FaPowerOff className="reg-circle-icon" /></a>
-                </li>
-            </ul>
-            <img src="/icons/wallet.svg" alt="profile" className="mobile-view-icons" />
-            {/* <FaPowerOff className="mobile-view-icons" /> */}
-            {/* {isShow ? <FaTimes className="hamburger" onClick={() => setIsShow(!isShow)} /> : <FaBars className="hamburger" onClick={() => setIsShow(!isShow)} />} */}
-
-            {/* <ul className={`mobile-navbar ${isShow?'show':'hide'}`}>
-                <li><a className="menu-link" href="/decentralized-trading/">Platform</a></li>
-                <li><a className="menu-link" href="/decentralized-trading/">Practice</a></li>
-                <li><a className="menu-link" href="/decentralized-trading/">Staking</a></li>
-                <li><a className="menu-link" href="/decentralized-trading/">Statistics</a></li>
-                <li><a className="menu-link" href="/decentralized-trading/">Documentation</a></li>
-                <li>
-                    <a className="button btn-pink menu-link" href="/decentralized-trading/">Buy $LVRJ" <FaExchangeAlt className="reg-circle-icon" /></a>
-                </li>
-                <li>
-                    <a className="button btn-pink menu-link" href="/decentralized-trading/">Connect Wallet <FaPowerOff className="reg-circle-icon" /></a>
-                </li>
-                <li>
-                    <div className="toggle-button-group">
-                        <span>Dark Mode</span> <Switch onChange={() => handleChangeMode(!mode)} checked={mode} />
+                    <div className="navbar-menu">
+                        <a className="menu-link" href="/decentralized-trading/">Platform</a>
+                        <a className="menu-link" href="/decentralized-trading/">Practice</a>
+                        <a className="menu-link" href="/decentralized-trading/">Staking</a>
+                        <a className="menu-link" href="/decentralized-trading/">Statistics</a>
+                        <a className="menu-link" href="/decentralized-trading/">Documentation</a>
                     </div>
-                </li>
-            </ul> */}
+                </div>
+
+                <ul className="menu-btns">
+                    <li>
+                        <div className="toggle-button-group">
+                            <span>Dark Mode</span> <Switch onChange={() => handleChangeMode(!mode)} checked={mode} />
+                        </div>
+                    </li>
+                    <li>
+                        <a className="button btn-pink" href="/decentralized-trading/">Buy $LVRJ" <FaExchangeAlt className="reg-circle-icon" /></a>
+                    </li>
+                    <li>
+                        <a className="button btn-pink" href="/decentralized-trading/">Connect Wallet <FaPowerOff className="reg-circle-icon" /></a>
+                    </li>
+                </ul>
+                <img src="/icons/wallet.svg" alt="profile" className="mobile-view-icons" />
+                {/* <FaPowerOff className="mobile-view-icons" /> */}
+                {/* {isShow ? <FaTimes className="hamburger" onClick={() => setIsShow(!isShow)} /> : <FaBars className="hamburger" onClick={() => setIsShow(!isShow)} />} */}
+
+                {/* <ul className={`mobile-navbar ${isShow?'show':'hide'}`}>
+                    <li><a className="menu-link" href="/decentralized-trading/">Platform</a></li>
+                    <li><a className="menu-link" href="/decentralized-trading/">Practice</a></li>
+                    <li><a className="menu-link" href="/decentralized-trading/">Staking</a></li>
+                    <li><a className="menu-link" href="/decentralized-trading/">Statistics</a></li>
+                    <li><a className="menu-link" href="/decentralized-trading/">Documentation</a></li>
+                    <li>
+                        <a className="button btn-pink menu-link" href="/decentralized-trading/">Buy $LVRJ" <FaExchangeAlt className="reg-circle-icon" /></a>
+                    </li>
+                    <li>
+                        <a className="button btn-pink menu-link" href="/decentralized-trading/">Connect Wallet <FaPowerOff className="reg-circle-icon" /></a>
+                    </li>
+                    <li>
+                        <div className="toggle-button-group">
+                            <span>Dark Mode</span> <Switch onChange={() => handleChangeMode(!mode)} checked={mode} />
+                        </div>
+                    </li>
+                </ul> */}
+            </div>
         </div>
+        
     );
 }
 
