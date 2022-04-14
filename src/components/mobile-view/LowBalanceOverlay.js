6d0180f5d0a7b22c90ba805168c9d304c0b70acb
Rsmile310 <rsmile310@gmail.com>
2022-04-15 02:52:41 +0900

    some changes-1

--- src/components/mobile-view/LowBalanceOverlay.js
+++ src/components/mobile-view/LowBalanceOverlay.js
@@ -3,7 +3,7 @@ import {AiOutlineClose} from "react-icons/ai"
 
 function LowBalanceOverlay({ onHidePaymentOverlay }) {
     return (
-        <div className="popup">
+        <div className="popup transparent">
             <div className="popup-header">
                 <AiOutlineClose onClick={onHidePaymentOverlay} className="popup-close" />
             </div>
@@ -21,7 +21,7 @@ function LowBalanceOverlay({ onHidePaymentOverlay }) {
               </div> 
               <div className="btn btn-deposit" style={{ margin: 'auto', color: 'white' }}>GO!</div>
             </div>
-            <img src="/icons/payment-back.png" alt="area_2" className="mini-chart" />
+            {/* <img src="/icons/payment-back.png" alt="area_2" className="mini-chart" /> */}
         </div>
     );
 }
